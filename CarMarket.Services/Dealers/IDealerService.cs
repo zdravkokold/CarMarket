33938deb2733c6999ceb4348dde777d3d1834bbe
Zdravko Koldzhiev <92548558+zdravkokold@users.noreply.github.com>
2022-12-10 14:24:46 +0200

    Roles Added to Dealer

---
+++ CarMarket.Services/Dealers/IDealerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMarket.Services.Dealers
+{
+    public interface IDealerService
+    {
+        Task<bool> ExistsById(string userId);
+
+        Task<bool> UserWithPhoneNumberExists(string phoneNumber);
+
+        Task<bool> UserHasCars(string userId);
+
+        Task Create(string userId, string phoneNumber);
+
+        Task<int> GetDealerId(string userId);
+    }
+}
